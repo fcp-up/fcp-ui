db68a63b05e2630beeecc6c6c7925a81e74977b8
lianguanfu <lianguanfu@agrithings.cn>
2017-07-23 10:10:49 +0800

    增加了地图功能

--- src/views/monitor/index.vue
+++ src/views/monitor/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="components-container" >
-    <code>
-      这里的所有的图表都基于ECharts,实例代码来源<a href='http://gallery.echartsjs.com/explore.html#sort=rank~timeframe=all~author=all' target='_blank'>gallery</a><br/>其实ECharts封装的很好了，用vue封装是很简单的事情，建议大家自己来封装。<a target='_blank' class='lin' href="https://segmentfault.com/a/1190000009762198#articleHeader16">相关教程</a>
-    </code>
+  <div class="monitor-container" >
+    <el-amap :vid="amapDemo"></el-amap>
   </div>
 </template>
+
+<<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .monitor-container {
+    width: 100%;
+    height:92vh;
+   }
+</style>
+
